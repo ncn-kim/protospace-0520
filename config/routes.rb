7081d5b87d3fe7790d278640caa0bfaacb1d011d
ncn-kim <ncn001001@gmail.com>
2026-05-20 11:31:57 +0900

    new protoボタンで投稿作成ページへ移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
-  resources :prototypes, only: [:index]
+  resources :prototypes, only: [:index, :new, :create]
 end
