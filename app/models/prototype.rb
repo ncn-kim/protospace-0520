8b4a03aff7ed009b972025e87e54719ad0d6eaea
ncn-kim <ncn001001@gmail.com>
2026-05-20 14:59:23 +0900

    all fin

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,12 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_one_attached :image
+
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
 end
